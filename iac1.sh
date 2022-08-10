0d2bd8da2e1ee2c8d3264860e900365544554c64
root <euraphael.dev@gmail.com>
2022-08-10 01:36:35 +0000

    Enviando meu arquivo de provisionamento via CLI

---
+++ iac1.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# CREATE DIRECTORIES
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+# CREATE GROUPS
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+# CREATE USERS
+
+echo "creating users now"
+
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd carlos -e
+
+useradd maria -c "Maria" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd maria -e
+
+useradd joao -c "Joao" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd joao -e
+
+useradd debora -c "Debora" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd debora -e
+
+useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd sebastiana -e
+
+useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd sebastiana -e
+
+useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd josefina -e
+
+useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd amanda -e
+
+useradd rogerio -c "Rogerio" -s /bin/bash -m -p $(openssl passwd Senha123)
+passwd rogerio -e
+
+echo "Users are created"
+
+# REGISTERING USERS AT OUR GROUPS
+
+echo "Registering users at GRP_ADM"
+usermod -G GRP_ADM carlos
+usermod -G GRP_ADM maria
+usermod -G GRP_ADM joao
+echo "Complete user register"
+
+echo "Registering users at GRP_VEN"
+usermod -G GRP_VEN debora
+usermod -G GRP_VEN sebastiana
+usermod -G GRP_VEN roberto
+echo "Complete user register"
+
+echo "Registering users at GRP_SEC"
+usermod -G GRP_SEC josefina
+usermod -G GRP_SEC amanda
+usermod -G GRP_SEC rogerio
+echo "Complete user register"
+
+echo "Managing permissions"
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+echo "Complete managing permissions"
+
+echo "Provisioning complete! Congratulations"
