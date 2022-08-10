31fe666e79af5a944061a01423520ded5ad3dd37
euraphaeldev <euraphael.dev@gmail.com>
2022-08-10 01:42:12 +0000

    Subindo arquivo de provisionamento via CLI

--- iac1.sh
+++ iac1.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # CREATE DIRECTORIES
-
+echo "Creating directories"
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
+echo "Directories are created now"
 
 # CREATE GROUPS
-
+echo "Creating groups"
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
+echo "Groups are created"
 
 # CREATE USERS
-
-echo "creating users now"
-
+echo "Creating users now"
 useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd Senha123)
 passwd carlos -e
 
@@ -43,28 +43,26 @@ passwd amanda -e
 
 useradd rogerio -c "Rogerio" -s /bin/bash -m -p $(openssl passwd Senha123)
 passwd rogerio -e
-
 echo "Users are created"
 
-# REGISTERING USERS AT OUR GROUPS
-
+# REGISTERING USERS AT YOUR GROUPS
 echo "Registering users at GRP_ADM"
 usermod -G GRP_ADM carlos
 usermod -G GRP_ADM maria
 usermod -G GRP_ADM joao
-echo "Complete user register"
+echo "Complete users register"
 
 echo "Registering users at GRP_VEN"
 usermod -G GRP_VEN debora
 usermod -G GRP_VEN sebastiana
 usermod -G GRP_VEN roberto
-echo "Complete user register"
+echo "Complete users register"
 
 echo "Registering users at GRP_SEC"
 usermod -G GRP_SEC josefina
 usermod -G GRP_SEC amanda
 usermod -G GRP_SEC rogerio
-echo "Complete user register"
+echo "Complete users register"
 
 echo "Managing permissions"
 chown root:GRP_ADM /adm
